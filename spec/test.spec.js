5224633164ac97bb4b89bb984fd3cc76350e5411
Oliver Kovacs <oliver@kinja.com>
2015-11-20 12:53:30 +0100

    Handle one book special case.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,7 +1,13 @@
 import {getprice} from './katapotter';
 
 describe('Katapotter price', () => {
+
 	it('should be 0 if no items in the basket', () => {
 		expect(getprice([])).toBe(0);
 	});
+
+	it('should be the same as the price of the item in the basket', () => {
+		expect(getprice([3])).toBe(8);
+	});
+
 });
--- src/katapotter.js
+++ src/katapotter.js
@@ -1,5 +1,11 @@
+const BOOKPRICE = 8;
+
 export function getprice(books) {
 	if (books.length === 0) {
 		return 0;
 	}
+
+  if (books.length === 1) {
+    return BOOKPRICE;
+  }
 }
