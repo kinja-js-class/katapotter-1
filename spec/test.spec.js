8b73137ddb08c5c4bab405ddf4070ff3a26e26ff
Istvan Bodnar <istvan@gawker.com>
2015-11-20 12:28:25 +0100

    FIRST TEST OHYEAH

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,5 +1,7 @@
-describe("A suite", () => {
-  it("contains spec with an expectation", () => {
-    expect(false).toBe(false);
-  });
+import {getprice} from './katapotter';
+
+describe('Katapotter price', () => {
+	it('should be 0 if no items in the basket', () => {
+		expect(getprice([])).toBe(0);
+	});
 });
