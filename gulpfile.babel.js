555e833be07ce519d843b31b968cd006c086bdf8
Oliver Kovacs <oliver@kinja.com>
2015-11-21 11:25:12 +0100

    Task to clean previous build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const jasmine = require('gulp-jasmine')
+const del = require('del')
 
-gulp.task('default', ['build'])
+gulp.task('default', ['clean-build', 'test'])
 
 gulp.task('build', _ => {
 	return gulp.src('src/**/*.js')
@@ -13,4 +14,8 @@ gulp.task('build', _ => {
 gulp.task('test', ['build'], _ => {
 	return gulp.src('spec/*.spec.js')
 		.pipe(jasmine())
+})
+
+gulp.task('clean-build', (callback) => {
+  del(['dist/*'], callback)
 })
