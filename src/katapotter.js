509e37d6ce6fcf27194f41b586627ac33d769f57
Istvan Bodnar <istvan@gawker.com>
2015-11-23 11:10:10 +0100

    Refactor

--- src/katapotter.js
+++ src/katapotter.js
@@ -3,17 +3,12 @@ import {_} from 'lodash';
 const BOOKPRICE = 8;
 const DISCOUNTS = [1, 0.95, 0.9, 0.8, 0.75];
 
-export function getprice(books) {
-	let uniqueBooksNum = _.unique(books).length;
-	if (books.length < 2) {
-		return BOOKPRICE * books.length;
-	}
-
+function getPermutations (books = [], limit = 2) {
 	let permutations = [[]];
 	for (let book of books) {
 		let inCart = false;
 		for (let p of permutations) {
-			if (!_.contains(p, book) && p.length < 5) {
+			if (!_.contains(p, book) && p.length < limit) {
 				inCart = true;
 				p.push(book);
 				break;
@@ -23,10 +18,22 @@ export function getprice(books) {
 			permutations.push([book]);
 		}
 	}
+	return permutations;
+}
 
-	let price = permutations.reduce((price, p) => {
+function getPermutationPrice(permutation) {
+	return permutation.reduce((price, p) => {
 		return price + p.length * BOOKPRICE * DISCOUNTS[p.length - 1];
 	}, 0);
+}
 
-	return price;
+export function getprice(books = []) {
+	if (!_.isArray(books) || books.length === 0) {
+		return 0;
+	}
+
+	let permutations = _.range(2, 5, 1).map(_.partial(getPermutations, books));
+	let prices = permutations.map(getPermutationPrice);
+	return _.min(prices);
+	
 }
