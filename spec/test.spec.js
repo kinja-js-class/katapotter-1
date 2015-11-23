1544768b88f4db8924eae813ac38b7e05005e172
Istvan Bodnar <istvan@gawker.com>
2015-11-23 01:56:39 +0100

    Added method to sum the number of books in the "basket", and return a discounted price for <= 5  different books

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,4 +1,5 @@
-import {getprice} from '../src/katapotter';
+import {getprice, getitemsum} from '../src/katapotter';
+import {BOOKPRICE, DISCOUNTS} from '../src/consts'
 
 describe('Katapotter price', () => {
 
@@ -7,7 +8,26 @@ describe('Katapotter price', () => {
 	});
 
 	it('should be the same as the price of the item in the basket', () => {
-		expect(getprice([3])).toBe(8);
+		expect(getprice([3])).toBe(BOOKPRICE);
 	});
+
+	it('should get proper discounts if less than 5 different books are in the basket', () => {
+		let bookset1 = [1, 2];
+		expect(getprice(bookset1)).toBe(bookset1.length * BOOKPRICE * DISCOUNTS[bookset1.length - 1]);
+		
+		let bookset2 = [1, 2, 1];
+		expect(getprice(bookset2)).not.toBe(bookset2.length * BOOKPRICE * DISCOUNTS[bookset2.length - 1]);
+	});
+
+
+	// misc test for the getitemsum ~private~ method
+	it('should get the sum of added items in the basket', () => {
+		let testcases = [
+			{source: [5, 2, 1, 1, 3, 2, 2, 3], expectation: [2, 3, 2, 0, 1]},
+			{source: [2, 1, 1, 3, 2, 2, 3], expectation: [2, 3, 2, 0, 0]},
+			{source: [4, 4, 4], expectation: [0, 0, 0, 3, 0]}
+		];
+		testcases.forEach((testcase) => expect(getitemsum(testcase.source)).toEqual(testcase.expectation));
+	})
 
 });
--- src/katapotter.js
+++ src/katapotter.js
@@ -1,7 +1,36 @@
-const BOOKPRICE = 8;
+// destructuring, default parameters, arrow functions, bónusz: iterátor spreaddel
+import countBy from 'lodash.countby'
+import arrayfrom from 'array.from'
+import {BOOKPRICE, DISCOUNTS} from './consts'
 
-export function getprice(books) {
+// no need to export this really, just left here so it can be tested
+export function getitemsum(items = []) {
+
+	let itemsums = countBy(items);
+	// note #1: Array.from was polyfilled :-/
+	// note #2: lodash countBy generates a string key no matter what, that's the reason for the string conversion
+	return Array.from(new Array(5), (v, index) => (itemsums['' + (index + 1)] || 0));
+
+	/*
+	// without Array.from:
+	return (Array.apply(null, Array(5))).map((value, index) => {
+		return itemsums[('' + (index + 1))] || 0;
+	});
+	*/
+}
+
+
+export function getprice(books = []) {
+	let booksum = getitemsum(books),
+		totalBooks = booksum.reduce((prevVal, currVal) => prevVal + currVal, 0);
+
+	// case #1: zero or one book, returning a non-discounted price
 	if (books.length < 2) {
 		return BOOKPRICE * books.length;
+	}
+
+	// case #2: maximum 5 books in the basket, each one different, returning discounted price
+	if (totalBooks <= 5 && Math.max(...booksum) < 2) {
+		return books.length * BOOKPRICE * DISCOUNTS[books.length - 1]
 	}
 }
