c2be5afe1170c7b32b4cedc0161d5f9cbd24bae5
Oliver Kovacs <oliver@kinja.com>
2015-11-23 09:32:54 +0100

    should be 16 on the 2 same items

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -6,8 +6,12 @@ describe('Katapotter price', () => {
 		expect(getprice([])).toBe(0);
 	});
 
-	it('should be the same as the price of the item in the basket', () => {
-		expect(getprice([3])).toBe(8);
+  it('should be 8 on 1 item', () => {
+    expect(getprice([3])).toBe(8);
+  });
+
+	it('should be 16 on the 2 same items', () => {
+		expect(getprice([3, 3])).toBe(16);
 	});
 
 });
--- src/katapotter.js
+++ src/katapotter.js
@@ -1,7 +1,13 @@
+import {_} from 'lodash';
+
 const BOOKPRICE = 8;
 
 export function getprice(books) {
 	if (books.length < 2) {
 		return BOOKPRICE * books.length;
 	}
+
+  if (_.unique(books).length === 1) {
+    return BOOKPRICE * books.length;
+  }
 }
