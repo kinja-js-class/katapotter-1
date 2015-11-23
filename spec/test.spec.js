d6b8aa3db722b407f2a5770419d32a8b92dec6fc
Istvan Bodnar <istvan@gawker.com>
2015-11-23 09:36:44 +0100

    Test 3 items in the basket

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -6,12 +6,16 @@ describe('Katapotter price', () => {
 		expect(getprice([])).toBe(0);
 	});
 
-  it('should be 8 on 1 item', () => {
-    expect(getprice([3])).toBe(8);
-  });
+	it('should be 8 on 1 item', () => {
+		expect(getprice([3])).toBe(8);
+	});
 
 	it('should be 16 on the 2 same items', () => {
 		expect(getprice([3, 3])).toBe(16);
+	});
+	
+	it('should be 24 for the same 3 items', () => {
+		expect(getprice([3, 3, 3])).toBe(24);
 	});
 
 });
